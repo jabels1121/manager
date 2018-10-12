4e4650443bb73388e1baf75da25a9c73123eae51
Jabels <jabels1121@gmail.com>
2018-10-12 14:52:28 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+    return(
+        <Text>This is manager App</Text>
+    );
+};
+
+export default App;
