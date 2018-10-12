57db70deef5c4d090d9353f5a4563f26b1fdb093
Jabels <jabels1121@gmail.com>
2018-10-12 17:20:27 +0300

    Created boilerplate of starter App component and created initialization of connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React,{Component} from 'react';
-import {Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
+import connectionConfig from './firebase/connectionConfig';
 
-const App = () => {
-    return(
-        <Text>This is manager App</Text>
-    );
-};
+
+class App extends Component{
+
+    componentWillMount(){
+        const config = connectionConfig;
+        firebase.initializeApp(config);
+        console.log(connectionConfig)
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>Hello!</Text>
+                </View>
+            </Provider>
+        );
+    }
+}
 
 export default App;
